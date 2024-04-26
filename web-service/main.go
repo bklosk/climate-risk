e696c83efdbebc4c148feb1a04d0a05169b58854
Ben Klosky <benklosky@uchicago.edu>
2024-04-26 15:09:24 +0000

    geolocation with openstreetmap integration seems to be working

--- web-service/main.go
+++ web-service/main.go
@@ -4,35 +4,36 @@ import (
 	"fmt"
 	"net/http"
 
+	"github.com/codingsince1985/geo-golang/openstreetmap"
 	"github.com/gin-gonic/gin"
 )
 
-// user attributes
-type user struct {
-	ID string `json:"id"`
-	Name string `json:"name"`
-	Email string `json:"email"`
-	Address string `json:"address"`
-	Latitude string `json:"latitude"`
-	Longitude string `json:"longitude"`
+const test_addr = "2426 ontario rd NW"
+
+func geolocate(address string) (location string) {
+	// geolocate user co-ords from address
+	var geocoder = openstreetmap.Geocoder()
+	coords, _ := geocoder.Geocode(test_addr)
+	if (coords != nil) {
+		fmt.Printf("%s location is (%.6f, %.6f)\n", test_addr, coords.Lat, coords.Lng)
+	}
+	
+	coord := "test"
+	return coord
 }
 
-var ben = user{
-		ID: "1",
-		Name: "Ben",
-		Email: "ben.klosky@gmail.com",
-		Address: "2426 ontario rd NW, Washington, DC 20009",	
-}
-
-func getBen(c *gin.Context) {
-	c.IndentedJSON(http.StatusOK, ben)
+func getTemp(c *gin.Context) {
+	// var user_address = c.Param("address")
+	temp := "temp_test"
+	c.IndentedJSON(http.StatusOK, temp)
 }
 
 func main() {
 	router := gin.Default()
-	router.GET("/ben", getBen)
+	router.GET("/temp/:address", getTemp)
+	geolocate(test_addr)
 
-	//START GIN SERVER
+	// start gin server
 	fmt.Println("Starting server on localhost:8080")
 	router.Run("localhost:8080")
 }
