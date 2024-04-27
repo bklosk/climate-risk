7adf4454739ba66baa58627b89eb6ffa272382a4
Ben Klosky <benklosky@uchicago.edu>
2024-04-27 23:22:08 +0000

    sexy, sexy tailwind in my html... in my javascript, with JSX. love react

--- webapp/src/App.jsx
+++ webapp/src/App.jsx
@@ -1,11 +1,11 @@
-import './style.css';
+import './index.css'
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Test App</h1>
+    <div className="mr-2">
+      <h1 className="text-3xl font-extrabold font-sans selection:bg-yellow-400">How will your climate change?</h1>
     </div>
   );
 }
@@ -13,3 +13,5 @@ function App() {
 const container = document.getElementById('root');
 const root = createRoot(container); // createRoot(container!) if you use TypeScript
 root.render(<App/>);
+
+export default App;
