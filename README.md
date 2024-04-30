fd4a6c9b773bec2491ba81a82c73256daa1c1494
Ben Klosky <benklosky@uchicago.edu>
2024-04-30 14:30:39 +0000

    good carpentry good cooking good mathematics

--- README.md
+++ README.md
@@ -1,8 +1,9 @@
 # climate-risk
-Quickly visualize climate risk for a given address in the US
+Webapp that uses climate models to predict how a user's microclimate will change
 
-/src holds a React / Tailwind app
-
-/web-service holds a Go (Gin) RESTful API that provides climate model projections
-
+/src holds a React / Tailwind app using D3 to visualize user outcomes\
+/web-service holds a Go (Gin) RESTful API: street address -> climate time-series\
+/web-service/utilities holds data modification utilities\
 /research holds climate models, sources, and thoughts
+
+React app (digitalocean) <-> Gin API (digitalocean) <-> Postgres(postGIS) (AWS)
--- web-service/main.go
+++ web-service/main.go
@@ -2,14 +2,27 @@ package main
 
 import (
 	"fmt"
+	"os"
+
+	"database/sql"
 	"net/http"
 
 	"github.com/codingsince1985/geo-golang/openstreetmap"
 	"github.com/gin-gonic/gin"
+	_ "github.com/lib/pq"
+)
+
+// connection string variables
+var (
+	host = os.Getenv("CLIMATE_DB_HOST")
+	port = 5432
+	user = os.Getenv("CLIMATE_DB_USER")
+	password = os.Getenv("CLIMATE_DB_PASSWORD")
+	dbname = "climate_data"
 )
 
 // getCoords: address -> lat, lng
-func getCoords(c *gin.Context) {
+func getCoords(c *gin.Context){
 	// spin up openstreetmap geocoder
 	var geocoder = openstreetmap.Geocoder()
 	var address = c.Param("address")
@@ -26,9 +39,33 @@ func getCoords(c *gin.Context) {
 	}
 }
 
+//wrapper for application-scoped DB connection
+func get_vector_tile(db *sql.DB) gin.HandlerFunc {
+    fn := func(c * gin.Context){
+		rows, err := db.Query(`SELECT ST_SkewX(rast) from "netcdf:test_data";`)
+		if err != nil {
+			fmt.Println(err)
+			c.JSON(http.StatusNotFound, gin.H{"error": "error"})
+		} else {
+			fmt.Println(rows)
+			c.JSON(http.StatusOK, gin.H{"rows": rows})
+		}
+	} 
+	return gin.HandlerFunc(fn)
+}
+
 func main() {
+	// connect to db
+	connection := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s", host, port, user, password, dbname)
+	db, err := sql.Open("postgres", connection)
+	if err != nil {
+		panic(err)
+	}
+
 	router := gin.Default()
 	router.GET("/coords/:address", getCoords)
+	router.GET("/tile/", get_vector_tile(db))
+
 
 	// start gin server
 	fmt.Println("\n\nStarting server on localhost:8080")
